1871eac2eb9c9fd395e55fe042e18911e3e56e84
Krzysztof Wędrowicz <krzysztof.wedrowicz@wp.pl>
2017-01-21 21:16:26 +0100

    added bower, gulp, bootstrap, jquery

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('default', ['css', 'js']);
+
+var styles = [
+    'bower_components/bootstrap/dist/css/bootstrap.min.css'
+];
+
+gulp.task('css', function(){
+    gulp.src(styles)
+        .pipe(concat('app.css'))
+        .pipe(cleanCSS())
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest('web/css'));
+});
+
+var assets = [
+    "bower_components/bootstrap/dist/js/bootstrap.min.js",
+    "bower_components/jquery/dist/jquery.min.js"
+];
+
+gulp.task('js', function () {
+    gulp.src(assets)
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('web/js'));
+});

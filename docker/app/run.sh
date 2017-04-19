28c865cb6ca1bfda3c741bac4b4f5593c6d408e4
Krzysztof Wędrowicz <krzysztof.wedrowicz@espeo.eu>
2017-04-19 12:06:22 +0200

    fixed permissions inside docker container

--- docker/app/run.sh
+++ docker/app/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
+
 php composer.phar install
 php bin/console doctrine:migrations:migrate
 php bin/console hautelook:fixtures:load

8fe82651787650714db4cd141d054b50e155d400
Krzysztof Wędrowicz <krzysztof.wedrowicz@espeo.eu>
2017-05-11 22:23:40 +0200

    removed unused statements

--- src/AppBundle/Form/Type/TaskTypeWithoutImage.php
+++ src/AppBundle/Form/Type/TaskTypeWithoutImage.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class TaskTypeWithoutImage extends AbstractType
 {

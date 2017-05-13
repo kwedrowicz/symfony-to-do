78af59b238896db62a61442772516e5c57db83dc
Krzysztof Wędrowicz <krzysztof.wedrowicz@espeo.eu>
2017-05-13 23:30:31 +0200

    task updatedAt as DateTime

--- src/AppBundle/Entity/Task.php
+++ src/AppBundle/Entity/Task.php
@@ -341,7 +341,7 @@ class Task
         if ($image) {
             // It is required that at least one field changes if you are using doctrine
             // otherwise the event listeners won't be called and the file is lost
-            $this->updatedAt = new \DateTimeImmutable();
+            $this->updatedAt = new \DateTime();
         }
     }
 

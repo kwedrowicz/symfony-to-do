ddbe830d1dfb13f66ddb0254819a95987b2b8432
Krzysztof Wędrowicz <krzysztof.wedrowicz@espeo.eu>
2017-05-21 17:09:08 +0200

    fixed tests

--- src/AppBundle/Controller/Web/TaskController.php
+++ src/AppBundle/Controller/Web/TaskController.php
@@ -29,20 +29,24 @@ class TaskController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $search = $request->query->get('search');
-        $finder = $this->get('fos_elastica.finder.app.task');
-        $boolQuery = new BoolQuery();
-        if($search){
-            $fieldQuery = new Query\MultiMatch();
-            $fieldQuery->setQuery($search);
-            $fieldQuery->setFields(['subject', 'category', 'tags']);
-            $boolQuery->addMust($fieldQuery);
-        }
-        $userFilter = new Query\Term();
-        $userFilter->setTerm('user', $this->getUser()->getUsername());
-        $boolQuery->addFilter($userFilter);
+        if($this->get('kernel')->getEnvironment() != 'test') {
+            $search = $request->query->get('search');
+            $finder = $this->get('fos_elastica.finder.app.task');
+            $boolQuery = new BoolQuery();
+            if ($search) {
+                $fieldQuery = new Query\MultiMatch();
+                $fieldQuery->setQuery($search);
+                $fieldQuery->setFields(['subject', 'category', 'tags']);
+                $boolQuery->addMust($fieldQuery);
+            }
+            $userFilter = new Query\Term();
+            $userFilter->setTerm('user', $this->getUser()->getUsername());
+            $boolQuery->addFilter($userFilter);
 
-        $tasks = $finder->find($boolQuery, 1000);
+            $tasks = $finder->find($boolQuery, 1000);
+        } else {
+            $tasks = $this->getUser()->getTasks();
+        }
 	    $deleteForms = [];
         /* @var Task $task */
 	    foreach ($tasks as $task) {
